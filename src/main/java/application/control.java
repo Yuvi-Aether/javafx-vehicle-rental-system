ff78ba56d7d8cf45a0a4d469c208cf4dcea06c2d
Yuvi Aether <shiyanimansukh75@gmail.com>
2026-03-26 14:18:02 +0530

    Added-Helper-Methodes

--- src/main/java/application/control.java
+++ src/main/java/application/control.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -28,60 +31,42 @@ public class control {
     @FXML
     PasswordField Password;
     @FXML
-    Label myLable;
+    Label myLabel;
 
-    public void login(ActionEvent e) throws Exception {
-        String user = Username.getText();
-        String pass = Password.getText();
-
-        LoginControl l = new LoginControl();
-        String s = l.Login(user, pass);
-        if (s.equals("Invalid Credentials")) {myLable.setText(s);return;}
-
-        if ("admin".equals(s)) {
-            Root = FXMLLoader.load(getClass().getResource("/Admin.fxml"));
-            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-
-            scene = new Scene(Root);
-
-            stage.setScene(scene);
-            stage.setTitle("Admin Panel");
-            stage.getIcons().add(
-                    new Image(getClass().getResource("/captain-puffy.jpg").toExternalForm())
-            );
-
-            stage.setFullScreen(true);
-            stage.setFullScreenExitHint("");
-            stage.show();
-        } else {
-            Root = FXMLLoader.load(getClass().getResource("/User.fxml"));
-            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-
-            scene = new Scene(Root);
-
-            stage.setScene(scene);
-            stage.setTitle("User Panel");
-            stage.getIcons().add(
-                    new Image(getClass().getResource("/captain-puffy.jpg").toExternalForm())
-            );
-
-            stage.setFullScreen(true);
-            stage.setFullScreenExitHint("");
-            stage.show();
+    public Parent loadFXML(String fxmlFile) throws RuntimeException,IOException {
+        URL resource = getClass().getResource(fxmlFile);
+        if (resource == null) {
+            throw new RuntimeException("FXML file not found: " + fxmlFile);
         }
+        return FXMLLoader.load(resource);
     }
 
-    public void Next(ActionEvent e) throws Exception {
-        Root = FXMLLoader.load(getClass().getResource("/Access.fxml"));
-        String css = this.getClass().getResource("/application.css").toExternalForm();
-
+    public void switchScene(ActionEvent e, String FXMLFile, String title, String css) {
+        try{
+        Root = loadFXML(FXMLFile);
+        }catch(IOException i){
+            System.err.println("File not found");
+        }catch(RuntimeException r){
+            System.err.println("Root must be not null");
+        }catch(Exception d){
+            System.err.println("Error!!");
+        }
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-
         scene = new Scene(Root);
-        scene.getStylesheets().add(css);
+
+        if (css != null) {
+            URL cssFile = getClass().getResource(css);
+
+            if (cssFile == null) {
+                throw new RuntimeException("File not found: " + css);
+            }
+
+            scene.getStylesheets().add(cssFile.toExternalForm());
+        }
 
         stage.setScene(scene);
-        stage.setTitle("Login Access");
+        stage.setTitle(title);
+
         stage.getIcons().add(
                 new Image(getClass().getResource("/captain-puffy.jpg").toExternalForm())
         );
@@ -89,6 +74,28 @@ public class control {
         stage.setFullScreen(true);
         stage.setFullScreenExitHint("");
         stage.show();
+    }
+
+    public void login(ActionEvent e) throws Exception {
+        String user = Username.getText();
+        String pass = Password.getText();
+
+        LoginControl log = new LoginControl();
+        String Role = log.Login(user, pass);
+        if (Role.equals("Invalid Credentials")) {myLabel.setText(Role);return;}
+
+        if ("admin".equals(Role)) {
+
+            switchScene(e, "/Admin.fxml", "Admin Panel", null);
+        } else {
+
+            switchScene(e, "/User.fxml", "User Panel", null);
+        }
+    }
+
+    public void Next(ActionEvent e) throws Exception {
+
+        switchScene(e, "/Access.fxml", "Login Access", "/application.css");
     }
 
     public void logout(ActionEvent e) {
