de8082b18660c0596f7f4af07ea1ede6c02d592e
Yuvi Aether <shiyanimansukh75@gmail.com>
2026-04-21 00:27:00 +0530

    new logic for rent

--- src/main/java/application/Car.java
+++ src/main/java/application/Car.java
@@ -6,11 +6,13 @@ class Car {
     String model;
     int pricePerDay;
     String status;
+    String currentRenter;
 
-    Car(String m, String p, String s, String i) {
+    Car(String m, String p, String s, String i,String r) {
         this.model = m;
         this.pricePerDay = Integer.parseInt(p);
         this.id = i;
+        this.currentRenter = r;
         this.status = s;
     }
 }
